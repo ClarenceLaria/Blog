a621790ecb056e95f6b4a7b422323e476afb5a76
ClarenceLaria <clarencelaria@gmail.com>
2023-10-10 13:33:46 +0300

    links commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import HomePage from './pages/Home.jsx';
+import Signup from './pages/signup.jsx';
+import Signin from './pages/signin.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "blog/",
+    element: <HomePage/>,
+  },
+  {
+    path: "signup/",
+    element: <Signup></Signup>,
+  },
+  {
+    path: "signin/",
+    element: <Signin/>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
+
+
