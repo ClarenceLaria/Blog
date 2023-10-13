428cb3cab57341e5a30ea4a7a2ae17fe7a4f4ae9
ClarenceLaria <clarencelaria@gmail.com>
2023-10-13 10:25:52 +0300

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="header  p-8 text-black text-sm pb-8 top-0 bg-white font-[Poppins] ">
       <div className="top flex justify-between">
         <div className="brand font-bold "><Link to="/">IDEA LAB</Link></div>
-        <nav className="navbar ">
+        <nav className="navbar hidden md:block lg:block ">
           <ul className="nav-list  gap-10 flex justify-between">
             <li className="nav-item cursor-pointer hover:text-orange-400 transition-all ease-in-out" ><Link to="/signup">Sign Up</Link></li>
             <li className="nav-item cursor-pointer hover:text-orange-400 transition-all ease-in-out"><Link to="/signin">Sign In</Link></li>
